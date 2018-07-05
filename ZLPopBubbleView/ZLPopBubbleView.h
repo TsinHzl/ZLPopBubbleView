32e12b57191137a6302deec21fc52b21de00f151
Zhonglin <TsinHzl@iCloud.com>
2018-07-05 13:42:32 +0800

    init repo.

---
+++ ZLPopBubbleView/ZLPopBubbleView.h
@@ -0,0 +1,29 @@
+//
+//  ZLPopBubbleView.h
+//  TYWJBus
+//
+//  Created by Harley He on 2018/6/20.
+//  Copyright © 2018 Harley He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef enum : NSUInteger {
+    ZLPopBubbleViewDirectionError = 0,
+    ZLPopBubbleViewDirectionTop = 1001,
+    ZLPopBubbleViewDirectionBottom
+} ZLPopBubbleViewDirection;
+
+@interface ZLPopBubbleView : UIView
+
+/* showingView */
+@property (strong, nonatomic) UIView *showingView;
+/* 箭头显示坐标 */
+@property (assign, nonatomic) CGFloat arrowPointX;
+/* ZLPopBubbleViewDirection */
+@property (assign, nonatomic) ZLPopBubbleViewDirection direction;
+/* viewColcor */
+@property (strong, nonatomic) UIColor *viewColor;
+/* contentView */
+@property (strong, nonatomic) UIView *contentView;
+@end
